df24e0ecfcf014050ead2462b8b011881c276774
ct <ct@gerudo.local>
2020-06-17 22:11:33 -0400

    add compiled files for deployment

---
+++ artist-list.d.ts
@@ -0,0 +1,3 @@
+import { Artist as RecentArtist } from './types/spotify-recent';
+import { Artist } from './types/spotify-top-tracks';
+export declare const generateArtistString: (artists: RecentArtist[] | Artist[]) => string;
---
+++ gatsby-node.d.ts
@@ -0,0 +1,15 @@
+import { TimeRange } from './spotify-api';
+export interface PluginOptions {
+    clientId: string;
+    clientSecret: string;
+    refreshToken: string;
+    timeRanges?: TimeRange[];
+    fetchPlaylists?: boolean;
+    fetchRecent?: boolean;
+}
+export declare const sourceNodes: ({ actions, createNodeId, store, cache }: {
+    actions: any;
+    createNodeId: any;
+    store: any;
+    cache: any;
+}, pluginOptions: PluginOptions) => Promise<void>;
